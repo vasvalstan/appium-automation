bc66215391115c28e54f1a8072fc963a6731241e
Valentin Stanciu <stanciu327@gmail.com>
2024-01-10 22:01:01 +0000

    starting saucelabs automation framework

--- src/main/java/InstallApplication.java
+++ src/main/java/InstallApplication.java
@@ -6,10 +6,9 @@ import java.net.URL;
 
 public class InstallApplication {
     public static void main(String[] args) throws MalformedURLException, InterruptedException {
-        System.out.println("Test");
         UiAutomator2Options dc = new UiAutomator2Options();
         dc.setDeviceName("appium-phone");
-        dc.setApp("C:\\Users\\User\\IdeaProjects\\appium\\ApiDemos-debug.apk");
+        dc.setApp("C:\\Users\\User\\IdeaProjects\\appium\\SauceLabs.apk");
         AndroidDriver driver = new AndroidDriver(new URL("http://0.0.0.0:4723"), dc);
 
         Thread.sleep(5000);
